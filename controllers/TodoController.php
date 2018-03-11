50f60fe1bd56bc7fa2232b8e7a2c64301b9c3f31
sudhir600 <sudhirgupta.456@gmail.com>
2018-03-11 11:08:09 +0530

    update 2: db query added and get response set in json instead xml

--- controllers/TodoController.php
+++ controllers/TodoController.php
@@ -2,16 +2,27 @@
 
 namespace app\controllers;
 
-// use Yii;
+use Yii;
+use yii\base\Model;
+use yii\web\NotFoundHttpException;
 use yii\rest\ActiveController;
+use app\models\Todo;
+// use yii\db\ActiveRecord;
 
 class TodoController extends ActiveController {
 	public $modelClass = 'app\models\Todo';
-	
+	/* 
+	public function beforeAction($action)
+	{
+		// use yii\web\Response; (add this namespace above)
+		// \Yii::$app->response->format = Response::FORMAT_JSON;
+		// return parent::beforeAction($action);
+	} 
+	*/
 	public function actions() {
 		$actions = parent::actions();
 		unset($actions['create']);
-		// unset($actions['test']);
+		unset($actions['index']);
 		return $actions;
 	}
 	
@@ -26,7 +37,22 @@ class TodoController extends ActiveController {
 	}
 	
 	public function actionIndex() {
-		echo 'index';
+		/*
+		Yii::$app->db->createCommand()->insert...
+		Yii::$app->db->createCommand()->batchInsert...
+		Yii::$app->db->createCommand()->update...
+		Yii::$app->db->createCommand()->delete...
+		Yii::$app->db->createCommand()->upsert... either insert or update if exits
+		*/
+		// update name for record 2
+		 // use excute for non returting query
+		$remark = 'this remark was set from PHP excute 4th time';
+		// Yii::$app->db->createCommand('UPDATE todo SET remark = "'.$remark.'" WHERE id=2')->execute();
+		// Yii::$app->db->createCommand()->update('todo', ['remark' => $remark, 'status' => 5], 'id = 2 and user = "Nehagupta"')->execute();
+		Yii::$app->db->createCommand()->update('todo', ['remark' => $remark, 'status' => 5], 'status = 3')->execute();
+		Yii::$app->db->createCommand()->delete('todo', 'id = 6')->execute();
+		$posts = Yii::$app->db->createCommand('SELECT * FROM todo')->queryAll();
+		print_r(json_encode($posts));	
 	}
 	
 }
